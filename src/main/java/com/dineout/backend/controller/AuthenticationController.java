28b103cdb9d75fb83e275af35e52fb5e6dcfe7a8
MJ <123522267+MJsica27@users.noreply.github.com>
2024-04-29 15:19:56 +0800

    add authenticationcontroller

---
+++ src/main/java/com/dineout/backend/controller/AuthenticationController.java
@@ -0,0 +1,65 @@
+package com.dineout.backend.controller;
+
+import com.dineout.backend.dto.request.LoginRequest;
+import com.dineout.backend.dto.request.RegisterRequest;
+import com.dineout.backend.dto.response.LoginResponse;
+import com.dineout.backend.entity.User;
+import com.dineout.backend.repository.UserRepository;
+import com.dineout.backend.service.AuthenticationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/auth")
+@Slf4j
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        try {
+            LoginResponse loginResponse = authenticationService.loginUser(loginRequest);
+            return new ResponseEntity<>(loginResponse, HttpStatus.OK);
+        } catch (UsernameNotFoundException e) {
+            return new ResponseEntity<>(Map.of("message", e.getMessage()), HttpStatus.FORBIDDEN);
+        } catch (Exception e) {
+            return new ResponseEntity<>(Map.of("message", e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest) {
+        try {
+            LoginResponse loginResponse = authenticationService.registerUser(registerRequest);
+            return new ResponseEntity<>(loginResponse, HttpStatus.OK);
+        } catch (EntityExistsException e) {
+            return new ResponseEntity<>(Map.of("message", e.getMessage()), HttpStatus.CONFLICT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(Map.of("message", e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/{userId}/user")
+    public ResponseEntity<?> updateUser(@PathVariable Long userId,
+            @RequestParam(name = "image", required = false) MultipartFile image,
+            @RequestParam(name = "user") String userJson) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            User user = objectMapper.readValue(userJson, User.class);
+            return new ResponseEntity<>(authenticationService.updateUser(userId, user, image), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(Map.of("message", e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+}
