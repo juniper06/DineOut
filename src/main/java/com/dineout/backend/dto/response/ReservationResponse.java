4f14e52d87ee776145800b7e5855c80147bde516
MJ <123522267+MJsica27@users.noreply.github.com>
2024-05-13 16:11:08 +0800

    reponses for Reservation and Restaurants

---
+++ src/main/java/com/dineout/backend/dto/response/ReservationResponse.java
@@ -0,0 +1,26 @@
+package com.dineout.backend.dto.response;
+
+import com.dineout.backend.entity.Reservation;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class ReservationResponse {
+    private Long id;
+    private Date reservationDate;
+    private String reservationTime;
+    private String note;
+    private Long countPeople;
+    private boolean cancelled;
+    private RestaurantResponse restaurant;
+
+    public ReservationResponse(Reservation reservation) {
+        id = reservation.getId();
+        reservationDate = reservation.getReservationDate();
+        reservationTime = reservation.getReservationTime();
+        note = reservation.getNote();
+        countPeople = reservation.getCountPeople();
+        cancelled = reservation.isCancelled();
+    }
+}
---
+++ src/main/java/com/dineout/backend/dto/response/RestaurantResponse.java
@@ -0,0 +1,34 @@
+package com.dineout.backend.dto.response;
+
+import com.dineout.backend.entity.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class RestaurantResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private String serviceHours;
+    private String address;
+    private List<Tag> tags;
+    private Type type;
+    private Cuisine cuisine;
+    private List<String> images;
+    private Long ratings;
+
+    public RestaurantResponse(Restaurant restaurant) {
+        id = restaurant.getId();
+        name = restaurant.getName();
+        description = restaurant.getDescription();
+        serviceHours = restaurant.getServiceHours();
+        address = restaurant.getLocation();
+        tags = restaurant.getTags();
+        type = restaurant.getType();
+        cuisine = restaurant.getCuisine();
+        images = restaurant.getImages();
+        ratings = restaurant.getAverageRating() == 0 ? 5 : restaurant.getAverageRating();
+    }
+}
