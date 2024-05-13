dd64536b3e03d2c6be59209d9bb8664dce670b7d
MJ <123522267+MJsica27@users.noreply.github.com>
2024-05-13 16:09:54 +0800

    add reservation Request

---
+++ src/main/java/com/dineout/backend/dto/request/ReservationRequest.java
@@ -0,0 +1,15 @@
+package com.dineout.backend.dto.request;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class ReservationRequest {
+    private Long restaurantId;
+    private Long userId;
+    private Date reservationDate;
+    private String note;
+    private String reservationTime;
+    private Long countPeople;
+}
---
+++ src/main/java/com/dineout/backend/entity/Reservation.java
@@ -0,0 +1,33 @@
+package com.dineout.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date reservationDate;
+    private String reservationTime;
+    @Column(columnDefinition = "TEXT")
+    private String note;
+    private Long countPeople;
+    private boolean cancelled = false;
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
